b59799210a5ed4ede84000ccacb0219652da802a
glennsvel90 <glennselwynvel@gmail.com>
2018-02-19 00:16:16 -0500

    db upto error message

--- navywDB.py
+++ navywDB.py
@@ -15,5 +15,56 @@ class NavyWDB():
         self.table = kwargs.get('table', 'weather')
         self.db = sqlite3.connect(self.filename)
         self.db.row_factory = sqlite3.Row
-        self.db.execute("""CREATE TABLE IF NOT EXIST {} (Date TEXT, time TEXT, status Text, Air_Temp FLOAT,
+        self.db.execute("""CREATE TABLE IF NOT EXIST {} (Date TEXT, Time TEXT, Status Text, Air_Temp FLOAT,
                             Barometric Press FLOAT, Wind_Speed FLOAT""".format(self.table))
+
+    def get_data_for_range(self, start, end):
+        """
+        Using the start and end dates, returns a generator of dictionaries that have all weather data, such as Wind Speed,
+        Barometric Pressure, and Wind Speed in those dates
+        """
+
+        dates_to_update = []
+
+        #find pre-2007 dates to update and add to list
+
+        for year in range(start.year, 2007):
+            if list(self._get_status_for_range(date(year,1,12), date(year,1,12))) == []:
+                dates_to_update.append(date(year,1,12))
+
+        #find post-2006 dates to update and add to list
+
+        if (end.year > 2006) and (start >= date(2007,1,1)):
+            temp_start = start
+        elif (end.year > 2006) and (start < date(2007,1,1)):
+            temp_start = date(2007,1,1)
+        else:
+            temp_start = end
+
+        #obtains a list of dates between temp_start and end
+
+        delta = end - temp_start
+        for d in range(delta.days +1):
+            dates_to_update.append(temp_start + timedelta(days=d))
+
+        statuses = list(self._get_status_for_range(temp_start, end))
+
+        for entry in statuses:
+            if entry['Status'] == 'COMPLETE':
+                dates_to_update.remove(datetime.strptime(str(entry['Date']), '%Y%m%d').date())
+            elif entry['Status'] == 'PARTIAL':
+                try:
+                    self._update_data_for_date(datetime.strptime(str(entry['Date']), '%Y%m%d').date(), True)
+                except:
+                    raise
+                dates_to_update.remove(datetime.strptime(str(entry['Date']), '%Y%m%d').date())
+
+        error_dates = []
+        for day in dates_to_update:
+            try:
+                self._update_data_for_date(day, False)
+            except ValueError as e:
+                error_dates.append(e)
+
+        if error_dates != []:
+            error
