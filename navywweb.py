ce6cf8a7455bc6f768b67ca072eba03905333879
glennsvel90 <glennselwynvel@gmail.com>
2018-02-21 20:27:43 -0500

    pre2007 helper function

--- navywweb.py
+++ navywweb.py
@@ -14,4 +14,21 @@ def _get_data_pre2007(date):
     '''Returns a generator of dicts of weather data for each year. For years from 2002 to 2006,
     the data is contained in one file for each year'''
 
-    
+    url = '{}/Environmental_Data_{}/'.format(BASE_URL, date.year)
+    print('Fetching online data for {} (full year)'.format(date.year))
+
+    try:
+        year_data = request.urlopen(url).read().decode(encoding='utf_8').split('\n')
+    except:
+        raise ValueError(date)
+    else:
+        year_data.pop(0)
+
+    for line in year_data:
+        elements = line.split()
+        yield dict(Date = elements[0],
+                   Time = elements[1],
+                   Status = 'COMPLETE',
+                   Air_Temp = elements[5],
+                   Barometric_Press = elements[7],
+                   Wind_Speed = elements[2])
